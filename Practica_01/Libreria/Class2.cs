679f8c70b3469d372c2d74a2678a6185688bc780
Rodrigo Iáñez Huertas <rih419@inlumine.ual.es>
2023-02-15 12:39:49 +0100

    Finalizacion Practica_01

---
+++ Practica_01/Libreria/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS.rih419.Practica_01
+{
+    internal class Class2
+    {
+    }
+}
---
+++ Practica_01/Testlibreria/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSS.rih419.Practica_01;
+
+namespace Testlibreria
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
