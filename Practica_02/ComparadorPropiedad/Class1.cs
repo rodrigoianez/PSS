f2a481ebfdc09bfb5966b57fc06ee222fdca61aa
Rodrigo Iáñez Huertas <rih419@inlumine.ual.es>
2023-03-16 12:00:54 +0100

    Comienzo parte 3 PropertyDescriptor Y Compare

---
+++ Practica_02/ComparadorPropiedad/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ComparadorPropiedad
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Practica_02/ComparadorPropiedad/ComparadorPropiedad.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparadorPropiedad
+{
+
+     
+    public class ComparadorPropiedad<T> : IComparer<T>
+    {
+       
+
+        PropertyDescriptor valor;
+
+
+        private PropertyDescriptor GetProperty(string name)
+        {
+            T item = (T)Activator.CreateInstance(typeof(T));
+            PropertyDescriptor propName = null;
+            foreach (PropertyDescriptor propDesc in TypeDescriptor.GetProperties(item))
+            {
+                if (propDesc.Name.Contains(name)) propName = propDesc;
+            }
+            return propName;
+        }
+        public int Compare(T? x, T? y)
+        {
+
+            ComparadorPropiedad<T> valorx = (ComparadorPropiedad<T>)valor.GetValue(x);
+            ComparadorPropiedad<T> valory = (ComparadorPropiedad<T>)valor.GetValue(y);
+
+
+            return valorx.Compare(valory););
+
+
+
+        }
+    }
+}
