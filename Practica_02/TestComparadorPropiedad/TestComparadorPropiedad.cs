da461621a01cf764165eb791f8659ca65803e245
Rodrigo Iáñez Huertas <rih419@inlumine.ual.es>
2023-03-17 13:22:24 +0100

    Primer test TestComparadorpropiedad (rojo)

---
+++ Practica_02/TestComparadorPropiedad/TestComparadorPropiedad.cs
@@ -0,0 +1,21 @@
+namespace PSS.rih419.Practica_02
+{
+    [TestClass]
+    public class TestComparadorPropiedad
+    {
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            UsuarioView usuario1 = new UsuarioView(0, "Rodrigo", "Comer", "Comida", true);
+            UsuarioView usuario2 = new UsuarioView(0, "Rodrigo", "Comer", "Comida", true);
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Rodrigo");
+
+            Assert.AreEqual(0,propiedad.Compare(usuario1, usuario2));
+
+        }
+    }
+}
