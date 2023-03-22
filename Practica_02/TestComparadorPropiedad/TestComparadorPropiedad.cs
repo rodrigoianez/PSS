72cbac16476c0a8556a33d53b611d7c5e5b08c80
Rodrigo Iáñez Huertas <rih419@inlumine.ual.es>
2023-03-22 10:28:43 +0100

    TestCompadorPropiedad acabado

--- Practica_02/TestComparadorPropiedad/TestComparadorPropiedad.cs
+++ Practica_02/TestComparadorPropiedad/TestComparadorPropiedad.cs
@@ -4,11 +4,13 @@ namespace PSS.rih419.Practica_02
     public class TestComparadorPropiedad
     {
         UsuarioView usuario1 = new UsuarioView(0, "Rodrigo", "Comer", "Comida", true);
-        UsuarioView usuario2 = new UsuarioView(1, "Rodrigo", "Comer", "Comida", false);
+        UsuarioView usuario2 = new UsuarioView(1, "Gelado", "Cenar", "Cena", false);
         UsuarioView usuario3 = null;
 
+        UsuarioView usuario4 = new UsuarioView(0, "Rodrigo", "Comer", "Comida", true);
+
         [TestMethod]
-        public void TestMethod1()
+        public void CompareId_UsuarioNull()
         {
 
             ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Id");
@@ -18,7 +20,7 @@ namespace PSS.rih419.Practica_02
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void CompareId_NullUsuario()
         {
 
             ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Id");
@@ -28,7 +30,7 @@ namespace PSS.rih419.Practica_02
         }
 
         [TestMethod]
-        public void TestMethod3()
+        public void CompareId_NullNull()
         {
 
             ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Id");
@@ -38,33 +40,236 @@ namespace PSS.rih419.Practica_02
         }
 
         [TestMethod]
-        public void TestMethod4()
+        public void CompareId_UsuarioUsuario_Menor()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Id");
+
+            Assert.AreEqual(-1, propiedad.Compare(usuario1, usuario2));
+
+        }
+
+        [TestMethod]
+        public void CompareId_UsuarioUsuario_Igual()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Id");
+
+            Assert.AreEqual(0, propiedad.Compare(usuario1, usuario4));
+
+        }
+
+        [TestMethod]
+        public void CompareId_UsuarioUsuario_Mayor()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Id");
+
+            Assert.AreEqual(1, propiedad.Compare(usuario2, usuario1));
+
+        }
+
+        [TestMethod]
+        public void CompareEsValido_UsuarioNull()
         {
 
             ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("EsValido");
 
-            Assert.AreEqual(-1, propiedad.Compare(usuario2, usuario1));
+            Assert.AreEqual(1, propiedad.Compare(usuario1, usuario3));
 
         }
 
         [TestMethod]
-        public void TestMethod5()
+        public void CompareEsValido_NullUsuario()
         {
 
-            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Id");
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("EsValido");
 
-            Assert.AreEqual(0, propiedad.Compare(usuario3, usuario3));
+            Assert.AreEqual(-1, propiedad.Compare(usuario3, usuario1));
 
         }
+
         [TestMethod]
-        public void TestMethod6()
+        public void CompareEsValido_NullNull()
         {
 
-            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Id");
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("EsValido");
 
             Assert.AreEqual(0, propiedad.Compare(usuario3, usuario3));
 
         }
+
+        [TestMethod]
+        public void CompareEsValido_UsuarioUsuario_Distintos()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("EsValido");
+
+            Assert.IsTrue(0 != propiedad.Compare(usuario1, usuario2));
+
+        }
+
+        [TestMethod]
+        public void CompareEsValido_UsuarioUsuario_Igual()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("EsValido");
+
+            Assert.AreEqual(0, propiedad.Compare(usuario1, usuario4));
+
+        }
+
+        [TestMethod]
+        public void CompareNombre_UsuarioNull()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Nombre");
+
+            Assert.AreEqual(1, propiedad.Compare(usuario1, usuario3));
+
+        }
+
+        [TestMethod]
+        public void CompareNombre_NullUsuario()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Nombre");
+
+            Assert.AreEqual(-1, propiedad.Compare(usuario3, usuario1));
+
+        }
+
+        [TestMethod]
+        public void CompareNombre_NullNull()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Nombre");
+
+            Assert.AreEqual(0, propiedad.Compare(usuario3, usuario3));
+
+        }
+
+        [TestMethod]
+        public void CompareNombre_UsuarioUsuario_Igual()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Nombre");
+
+            Assert.AreEqual(0, propiedad.Compare(usuario1, usuario4));
+
+        }
+
+        [TestMethod]
+        public void CompareNombre_UsuarioUsuario_Distinto()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Nombre");
+
+            Assert.IsTrue(0 != propiedad.Compare(usuario1, usuario2));
+
+        }
+
+        [TestMethod]
+        public void ComparePalabraPaso_UsuarioNull()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("PalabraPaso");
+
+            Assert.AreEqual(1, propiedad.Compare(usuario1, usuario3));
+
+        }
+
+        [TestMethod]
+        public void ComparePalabraPaso_NullUsuario()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("PalabraPaso");
+
+            Assert.AreEqual(-1, propiedad.Compare(usuario3, usuario1));
+
+        }
+
+        [TestMethod]
+        public void ComparePalabraPaso_NullNull()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("PalabraPaso");
+
+            Assert.AreEqual(0, propiedad.Compare(usuario3, usuario3));
+
+        }
+
+        [TestMethod]
+        public void ComparePalabraPaso_UsuarioUsuario_Igual()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("PalabraPaso");
+
+            Assert.AreEqual(0, propiedad.Compare(usuario1, usuario4));
+
+        }
+
+        [TestMethod]
+        public void ComparePalabraPaso_UsuarioUsuario_Distinto()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("PalabraPaso");
+
+            Assert.IsTrue(0 != propiedad.Compare(usuario1, usuario2));
+
+        }
+
+        [TestMethod]
+        public void CompareCategoria_UsuarioNull()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Categoria");
+
+            Assert.AreEqual(1, propiedad.Compare(usuario1, usuario3));
+
+        }
+
+        [TestMethod]
+        public void CompareCategoria_NullUsuario()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Categoria");
+
+            Assert.AreEqual(-1, propiedad.Compare(usuario3, usuario1));
+
+        }
+
+        [TestMethod]
+        public void CompareCategoria_NullNull()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Categoria");
+
+            Assert.AreEqual(0, propiedad.Compare(usuario3, usuario3));
+
+        }
+
+        [TestMethod]
+        public void CompareCategoria_UsuarioUsuario_Igual()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Categoria");
+
+            Assert.AreEqual(0, propiedad.Compare(usuario1, usuario4));
+
+        }
+
+        [TestMethod]
+        public void CompareCataegoria_UsuarioUsuario_Distinto()
+        {
+
+            ComparadorPropiedad<UsuarioView> propiedad = new ComparadorPropiedad<UsuarioView>("Categoria");
+
+            Assert.IsTrue(0 != propiedad.Compare(usuario1, usuario2));
+
+        }
+
+
 
     }
 }
