1be0d967595789913defb3569c31f78e187f841a
Rodrigo Iáñez Huertas <rih419@inlumine.ual.es>
2023-03-29 10:46:15 +0200

    Clase ficha creada constructores y enum

---
+++ Practica_03/Practica_03/Ficha.cs
@@ -0,0 +1,25 @@
+﻿namespace PSS.rih419.Practica_03
+{
+    public class Ficha
+    {
+
+        public ColorFicha _color;
+
+        public ColorFicha Color { get { return _color; } }
+
+        public Ficha()
+        {
+        }
+
+        public Ficha(ColorFicha color)
+        {
+            _color = color;
+        }
+    }
+
+    public enum ColorFicha
+    {
+        Rojo, Negro, SinColor
+
+    }
+}
