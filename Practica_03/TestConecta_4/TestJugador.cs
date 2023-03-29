440682adaa42d776cfaba9ebd8f764f038480705
Rodrigo Iáñez Huertas <rih419@inlumine.ual.es>
2023-03-29 20:52:17 +0200

    TestJugador tests creados verdes hasta fin video1

--- Practica_03/TestConecta_4/TestJugador.cs
+++ Practica_03/TestConecta_4/TestJugador.cs
@@ -1,4 +1,5 @@
 using PSS.rih419.Practica_03;
+using System.Diagnostics.SymbolStore;
 
 namespace PSS.rih419.Practica_03
 {
@@ -6,15 +7,48 @@ namespace PSS.rih419.Practica_03
     public class TestJugador
     {
         [TestMethod]
-        public void CrearJugadorTest()
+        public void Constructor_SinParametros_EsNoNulo()
         {
             Jugador jugador = new Jugador();
-            Assert.IsNotNull(jugador); 
+            Assert.IsNotNull(jugador);
         }
 
-        
+        [TestMethod]
+        public void Constructor_ParametroNombre_EsIgual() 
+        {
+            Jugador jugador = new Jugador("Nombre jugador");
+            bool booleano = jugador.Nombre == ("Nombre jugador"); 
+            Assert.IsTrue(booleano);
+        }
 
+        [TestMethod]
+        public void Constructor_SinParametros_NombreEsNombreInstancia()
+        {
+            Jugador.numJugadores = 0;
+            Jugador jugador = new Jugador();
+            bool booleano = (jugador.Nombre == ("Jugador1"));
+            Assert.IsTrue(booleano);
+        }
+
+        [TestMethod]
+        public void Ficha_ColorRojo_ColorFichaJugadorEsIgual()
+        {
+            Jugador jugador = new Jugador();
+            jugador.Ficha = new Ficha(ColorFicha.Rojo);
+            bool booleano = (jugador.Ficha.Color == ColorFicha.Rojo);
+            Assert.IsTrue(booleano);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Ficha_No_Asignada_Excepcion()
+        {
+
+            Jugador jugador = new Jugador();
+            bool boleano = (jugador.Ficha.Color == ColorFicha.Rojo);
+            Assert.Fail("Se esperaba una ArgumentException");
+
+        }
     }
 
-    
 }
