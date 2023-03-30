359d43da19b727e52cc681b40874edf210c63dba
Rodrigo Iáñez Huertas <rih419@inlumine.ual.es>
2023-03-30 22:55:43 +0200

    Clase posicoin creada para permitir el metodo PonerFichaPosicion

---
+++ Practica_03/Practica_03/Posicion.cs
@@ -0,0 +1,14 @@
+﻿namespace PSS.rih419.Practica_03
+{
+    public struct Posicion
+    {
+        public int Fila;
+        public int Columna;
+
+        public Posicion(int fila, int columna)
+        {
+            this.Fila = fila;
+            this.Columna = columna;
+        }
+    }
+}
